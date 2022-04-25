c53bd46b6b04ae0dae22a250e9efa9f7e83646ee
Josue Martinez <j.e.martinez1993@gmail.com>
2022-04-25 11:47:09 +0200

    removed useEffect from profileimages component

--- src/components/ProfileImages/profileimages.component.jsx
+++ src/components/ProfileImages/profileimages.component.jsx
@@ -2,15 +2,7 @@ import { useEffect } from "react"
 import {useNavigate} from "react-router-dom"
 import "./profileimages.styles.css";
 
-const ProfileImages = ({open}) => {
-    const check = useNavigate();
-
-    useEffect(() => {
-        if (!open) {
-        // check.push("/")
-        }
-    }, [open, check]
-    );
+const ProfileImages = () => {
 
   return (
     <div>
